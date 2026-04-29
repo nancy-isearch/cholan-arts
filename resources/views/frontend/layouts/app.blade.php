e8f2c67ca43e454be14c9289a205f9530a43ffea
nancy-isearch <nancy@isearchsolution.com>
2026-04-29 14:53:21 +0530

    Worked on google tags and sitemap

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -2,6 +2,13 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PSL5FKDQ');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('seo_title', 'Cholan Arts')</title>
@@ -76,14 +83,26 @@
         @endif
     @endif
 </head>
+<!-- Google tag (gtag.js) start --> 
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-449V2HLR8Q"></script> 
+<script>   
+window.dataLayer = window.dataLayer || [];   
+function gtag(){dataLayer.push(arguments);}   
+gtag('js', new Date());   
+gtag('config', 'G-449V2HLR8Q'); 
+</script>
+<!-- Google tag (gtag.js) end -->
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PSL5FKDQ"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @include('frontend.components.header')
-
     @yield('content')
-
     @include('frontend.components.footer')
-
 
     @if ($seo && !empty($seo->schema_json))
         @if ($seo->page_key == 'home')
